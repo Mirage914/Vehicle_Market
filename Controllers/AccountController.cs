af77c679f8f5f93a691b790ee91240d0d9526b20
Mirage914 <76565864+Mirage914@users.noreply.github.com>
2021-09-19 01:01:48 +0200

    Add Login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using MySqlX.XDevAPI;
 using Project1.Data;
 using Project1.Models;
 
@@ -19,7 +22,7 @@ namespace Project1.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index([Bind("ID,UserName,Password")] Account account)
+        public IActionResult Register()
         {
 
             return View();
@@ -29,14 +32,46 @@ namespace Project1.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Register(Account account)
         {
-                  if (ModelState.IsValid)
-                    {
-                     obj.Account.Add(account);
-                     obj.SaveChanges();
-                   }
-            return RedirectToAction("","");
+            if (account.UserName == null || account.Password == null)
+            {
+                return View();
+
+            }
+            else if (ModelState.IsValid)
+            {
+                obj.Account.Add(account);
+                obj.SaveChanges();
+            }
+            return RedirectToAction("", "");
         }
-        
-        
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(Account account)
+        {
+            if (account.UserName == null || account.Password == null)
+            {
+                return View();
+            }
+            var user = obj.Account.Where(x => x.UserName == account.UserName && x.Password == account.Password).FirstOrDefault();
+            if (user == null)
+            {
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("", "");
+
+            }
+        }
+
+
     }
 }
--- Models/Account.cs
+++ Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ using System.Threading.Tasks;
     public class Account
 
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage="Please enter Username.")]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "Please enter Password.")]
         public string Password { get; set; }
     }
 }
